24715e12008a42d01ed161438d0f416098220f34
Michel Detiilli <detiillimichel@gmail.com>
2026-03-21 11:51:19 -0300

    Update layout.tsx

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,17 +1,13 @@
-import { checkLoggedIn } from 'utils/useUser'
-import Image from 'next/image'
-export default function layout({ children }: { children: React.ReactNode }) {
-  checkLoggedIn()
+import React from 'react'
+
+export default function AccountLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <div className="absolute top-0 left-0 w-full min-h-full bg-white z-40">
-      <div className="grid grid-cols-1  sm:gap-6 sm:grid-cols-2">
-        <div className="relative w-full min-h-screen hidden sm:block ">
-          <Image src={'/ig-cover.jpg'} fill alt="" className=" object-cover" />
-        </div>
-        <div className="p-4 py-16 max-w-[480px] mx-auto h-screen flex items-center justify-center">
-          {children}
-        </div>
-      </div>
+    <div className="min-h-screen bg-black">
+      {children}
     </div>
   )
 }
